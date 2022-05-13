9ebfe3648ba7a1bb6a04eee557e27e071bb9d39b
Sergio Andrés Martínez <elsamplio@msn.com>
2022-05-13 13:32:11 -0500

    initial commit

---
+++ apps/samp-cv-monorepo/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'samp-cv-monorepo',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/samp-cv-monorepo',
+};
